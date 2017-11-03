1d0b4262ed97eabfdc767b09e1d121190de98b90
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-03 03:28:52 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,13 +8,3 @@ def my_collect(array)
  end
  collection
 end
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
